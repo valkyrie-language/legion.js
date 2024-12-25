7e380a41991c055e48dee862222a7e6ee416f3b6
Aster <galaster@foxmail.com>
2024-12-25 00:08:25 +0000

    🔮 Replace binary with wasi

--- packages/legion-auto/legion.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-import {runLegion} from "./index.js";
-// skip 2, 0='node', 1='legion.js'
-await runLegion(process.argv.slice(2))
--- packages/legion-darwin-arm64/index.js
+++
@@ -1,5 +0,0 @@
-import {fileURLToPath} from 'node:url'
-import {dirname, join} from 'node:path'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
-export const location = join(__dirname, 'legion-win32-x64.exe')
