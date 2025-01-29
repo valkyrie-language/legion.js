f49a19268cf86a276c29b016ed087c2803d9bd63
Aster <galaster@foxmail.com>
2025-01-29 14:38:11 +0800

    Add the npx project creator

--- packages/legion-cli/tests/polyfill/decode_component/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { WasiCliStdout } from './interfaces/wasi-cli-stdout.js';
-import { WasiIoError } from './interfaces/wasi-io-error.js';
-import { WasiIoStreams } from './interfaces/wasi-io-streams.js';
---
+++ tests/polyfill/decode_component/index.d.ts
@@ -0,0 +1,3 @@
+import { WasiCliStdout } from './interfaces/wasi-cli-stdout.js';
+import {WasiIoError} from './interfaces/wasi-io-error.js';
+import {WasiIoStreams} from './interfaces/wasi-io-streams.js';
